daf84f922faa9645c837baa7bd15c4f25d4f244f
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-05-19 12:55:59 -0400

    initial

---
+++ DualMotorController.cpp
@@ -0,0 +1,90 @@
+#include "WConstants.h"
+#include "DualMotorController.h"
+
+DualMotorController::DualMotorController(AF_DCMotor &left_motor, AF_DCMotor &right_motor) {
+  _left_motor = *left_motor;
+  _right_motor = *right_motor;
+  speed = 0;
+  _forward = 1;
+  current_direction = &DualMotorController::straight;
+}
+
+void DualMotorController::left() {
+  if (_forward) {
+    _left_motor->run(FORWARD);
+    _right_motor->run(BACKWARD);
+  } else {
+    _left_motor->run(BACKWARD);
+    _right_motor->run(FORWARD);
+  }
+  current_direction = &DualMotorController::left;
+}
+
+void DualMotorController::right() {
+  if (_forward) {
+    _left_motor->run(BACKWARD);
+    _right_motor->run(FORWARD);
+  } else {
+    _left_motor->run(FORWARD);
+    _right_motor->run(BACKWARD);
+  }
+  current_direction = &DualMotorController::right;
+}
+
+void DualMotorController::straight() {
+  if (_forward) {
+    _left_motor->run(FORWARD);
+    _right_motor->run(FORWARD);
+  } else {
+    _left_motor->run(BACKWARD);
+    _right_motor->run(BACKWARD);
+  }
+  current_direction = &DualMotorController::straight;
+}
+
+void DualMotorController::stop() {
+  _left_motor->run(RELEASE);
+  _right_motor->run(RELEASE);
+  speed = 0;  
+  current_direction = &DualMotorController::stop;
+}
+
+void DualMotorController::forward() {
+  _forward = 1;
+  (this->*current_direction)();
+}
+
+void DualMotorController::reverse() {
+  _forward = 0;
+  (this->*current_direction)();
+}
+
+void DualMotorController::increaseSpeed() {
+  // TODO Implement speed increasing
+  speed = 255;
+  _left_motor->setSpeed(speed);
+  _right_motor->setSpeed(speed);
+}
+
+void DualMotorController::decreaseSpeed() {
+  // TODO Implement speed decreasing
+  speed = 0;
+  _left_motor->setSpeed(speed);
+  _right_motor->setSpeed(speed);
+}
+
+void DualMotorController::setSpeed(int new_speed) {
+  speed = new_speed;
+  _left_motor->setSpeed(speed);
+  _right_motor->setSpeed(speed);
+}
+
+int DualMotorController::isReversing() {
+  return (_forward == 0);
+}
+
+void DualMotorController::reset() {
+  current_direction = &DualMotorController::straight;
+  _forward = 1;
+  speed = 0;
+}
---
+++ DualMotorController.h
@@ -0,0 +1,29 @@
+#ifndef DualMotorController_h
+#define DualMotorController_h
+#include <../AFMotor/AFMotor.h>
+
+class DualMotorController
+{
+  private:
+    AF_DCMotor *_left_motor;
+    AF_DCMotor *_right_motor;
+    int speed;
+    int _forward;
+    void (DualMotorController::*current_direction)();
+  public:
+    DualMotorController(AF_DCMotor&, AF_DCMotor&);
+    void left();
+    void right();
+    void straight();
+    void stop();
+    void forward();
+    void reverse();
+    void increaseSpeed();
+    void decreaseSpeed();
+    void setSpeed(int);
+    int isReversing();
+    void reset();
+};
+
+#endif
+
